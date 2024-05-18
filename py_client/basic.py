8113618cd958be7eed688197cd94e762771b5729
Kem Guianalan <guianalankem@gmail.com>
2024-05-18 16:58:54 +0800

    task: echo GET Data

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,9 +4,13 @@ import requests
 #endpoint = "http://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "hello world", "method": "GET"})
+get_response = requests.get(endpoint, params={"abc": "cba"}, json={"query": "hello world"})
 
-print(get_response.json()["message"])
+#print(get_response.text)
 #print(get_response.status_code)
+
+#print(get_response.json()["message"])
+print(get_response.json())
+
 
 
