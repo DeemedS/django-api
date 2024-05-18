1d9adf5c14888b80bb90e8eaa8de529cd728f70c
Kem Guianalan <guianalankem@gmail.com>
2024-05-18 16:28:46 +0800

    task: Initial API View

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,12 @@
 import requests
 
 #endpoint = "http://httpbin.org/status/200"
-endpoint = "http://httpbin.org/anything"
+#endpoint = "http://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
 
 get_response = requests.get(endpoint, json={"query": "hello world", "method": "GET"})
 
-print(get_response.json())
-print(get_response.status_code)
+print(get_response.json()["message"])
+#print(get_response.status_code)
 
 
